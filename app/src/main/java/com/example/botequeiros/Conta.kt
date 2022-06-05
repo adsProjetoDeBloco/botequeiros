9469cc295576291f0b600872710343efe65c2837
Pedro Humberto <pedrohlucape@gmail.com>
2022-06-05 19:00:52 -0300

    interface e gorjeta

--- app/src/main/java/com/example/botequeiros/Conta.kt
+++ app/src/main/java/com/example/botequeiros/Conta.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.SeekBar
 import android.widget.TextView
 import org.w3c.dom.Text
 
@@ -16,6 +17,19 @@ class Conta : AppCompatActivity() {
         val getPreco = findViewById<TextView>(R.id.txt_preco)
         val getQuantidade = findViewById<TextView>(R.id.txt_quantidade)
         val btnFecharConta = findViewById<Button>(R.id.btn_dividir)
+        val getPorcentagem = findViewById<TextView>(R.id.txt_porcentagem)
+
+        val getSeekBar = findViewById<SeekBar>(R.id.txt_seekBar)
+        getSeekBar.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener{
+            override fun onProgressChanged(p0: SeekBar?, progress: Int, p2: Boolean) {
+                getPorcentagem.text = progress.toString()
+            }
+
+            override fun onStartTrackingTouch(p0: SeekBar?) {
+            }
+            override fun onStopTrackingTouch(p0: SeekBar?) {
+            }
+        })
 
 
 
@@ -23,8 +37,10 @@ class Conta : AppCompatActivity() {
             val pessoas = getPessoas.toString().toInt()
             val preco = getPreco.text.toString().toFloat()
             val quantidade = getQuantidade.text.toString().toInt()
+            val gorjeta = getPorcentagem.text.toString().toFloat()
 
-            var total = preco * quantidade
+
+            var total = (preco * quantidade) * (1 + gorjeta / 100)
             var divisao = total / pessoas
 
             val intentComanda = Intent(this, Comanda::class.java)
